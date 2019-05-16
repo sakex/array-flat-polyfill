8e9a4906b8b9fcf26626e9f453a92b50300c5ff4
Alexandre <alexandre.senges@ermeo.com>
2019-05-16 14:16:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+(function() {
+  if (!Array.prototype.flat) {
+    Array.prototype.flat = function(depth) {
+      if (!depth) depth = 1;
+      else if (typeof depth !== 'number') {
+        try {
+          depth = parseInt(depth, 10);
+        } catch (err) {
+          depth = 1;
+        }
+      }
+      var length = this.length;
+      var elem;
+      var newArr = [];
+      for (var i = 0; i < length; ++i) {
+        elem = this[i];
+        if (depth >= 0 && Array.isArray(elem)) {
+          var nestedArray = elem.flat(--depth);
+          for (var j = 0; j < nestedArray.length; ++j) {
+            newArr.push(nestedArray[j]);
+          }
+        } else newArr.push(elem);
+      }
+      return newArr;
+    };
+  }
+})();
